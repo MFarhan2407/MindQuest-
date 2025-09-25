60331ae067372aabb8966d77021a2f2bbf8e645a
Farhan <mf6819042@gmail.com>
2025-09-25 14:13:42 +0700

    merging

--- controllers/eduController.js
+++ controllers/eduController.js
@@ -8,7 +8,20 @@ class eduController {
             const user = await User.findOne({
                 where: { id }
             })
-            res.render("edu-dashboard", { user })
+
+            const students = await User.findAll({
+                where: {
+                    role: "STUDENT"
+                }
+            })
+            const profile = await Profile.findAll()
+            const subjects = await Subject.findAll()
+            const challenge = await Challenge.findAll()
+
+            // console.log(challenge.length);
+
+
+            res.render("edu-dashboard", { user, students, profile, subjects, challenge })
         } catch (error) {
             res.send(error)
         }
@@ -22,8 +35,11 @@ class eduController {
             })
 
             const subject = await Subject.findAll()
+            const challenges = await Challenge.findAll({
+                include: Subject
+            })
 
-            res.render("edu-area", { user, subject })
+            res.render("edu-area", { user, subject, challenges })
         } catch (error) {
             res.send(error)
         }
@@ -34,7 +50,7 @@ class eduController {
             const { question, correctAnswer, choices, subjects } = req.body
 
             if (!req.session.userId) {
-                return res.redirect('/login') 
+                return res.redirect('/login')
             }
 
 
@@ -62,6 +78,99 @@ class eduController {
         }
     }
 
+    static async myStudents(req, res) {
+        try {
+
+            const students = await User.findAll()
+            const profile = await Profile.findAll()
+            res.render("my-students", { students, profile })
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async deleteQuestion(req, res) {
+        try {
+            const { id } = req.params
+
+            await Challenge.destroy({
+                where: {
+                    id: id
+                }
+            })
+
+            res.redirect("/mindquest/educator/area")
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async showEditFormPage(req, res) {
+        try {
+            const { id } = req.params
+            const userId = req.session.userId
+            
+            const user = await User.findOne({
+                include: {
+                    model: Profile,
+                    where: {
+                        id: id
+                    }
+                }
+            })
+            
+            const profile = await Profile.findOne({
+                where: {
+                    UserId: userId
+                }
+            })
+            // console.log(profile);
+            
+            
+            
+            
+            res.render("edu-profile-edit", { user, profile })
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async editProfile(req, res) {
+        try {
+
+            const userId = req.params.id;
+            const { name, email, address, bio } = req.body
+
+            if (req.file) {
+                avatarPath = '/uploads/' + req.file.filename;
+            }
+
+
+            const profile = await Profile.findOne({ where: { UserId: userId } });
+
+            if (profile) {
+                await profile.update({
+                    name,
+                    email,
+                    address,
+                    bio,
+                    ...(avatarPath && { avatar: avatarPath })
+                });
+            } else {
+                await Profile.create({
+                    UserId: userId,
+                    address,
+                    bio,
+                    avatar: avatarPath
+                });
+            }
+
+            res.redirect('/mindquest/educator/profile');
+        } catch (error) {
+            res.send(error)
+        }
+    }
 }
 
 module.exports = eduController
--- route/eduRoutes.js
+++ route/eduRoutes.js
@@ -9,4 +9,12 @@ route.get("/educator/area", eduController.areaEducator)
 
 route.post("/educator/question/create", eduController.createQuestion)
 
+route.get("/educator/student/area", eduController.myStudents)
+
+route.post("/educator/question/:id/delete", eduController.deleteQuestion)
+
+route.get("/educator/profile/:id/edit", eduController.showEditFormPage)
+
+route.post("/educator/profile/:id/edit", eduController.editProfile)
+
 module.exports = route
